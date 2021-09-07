1c691430bbd420219ce4bd86359a26f0abdeeba2
Digital-Thought <matthew@digital-thought.org>
2021-09-07 22:32:51 +1000

    New date/time format added

--- digital_thought_commons/internet/scrapper.py
+++ digital_thought_commons/internet/scrapper.py
@@ -49,6 +49,7 @@ class Scrapper(object):
         options.add_argument(f"user-data-dir={self.data_dir}")
         if self.headless:
             options.add_argument('--headless')
+            options.add_argument('user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36')
             options.add_argument('--disable-gpu')
             options.headless = True
         if proxy:
