5b4991c35b0e36391bcf0e8fb48cbb4aefbfa32f
Digital-Thought <matthew@digital-thought.org>
2021-09-28 15:24:15 +1000

    Default Application Initialisation Class

--- digital_thought_commons/app/__init__.py
+++ digital_thought_commons/app/__init__.py
@@ -7,6 +7,39 @@ from argparse import ArgumentParser
 from digital_thought_commons import logging as logger
 from art import *
 
+from flask import Flask
+
+
+class AbstractFlaskApp(Flask):
+
+    def __init__(self, app_spec_path: str = None) -> None:
+        if app_spec_path is None or not os.path.exists(app_spec_path):
+            raise Exception(f"Invalid app_spec_path provided: {app_spec_path}")
+
+        with open(app_spec_path, "r") as spec:
+            self.app_spec = json.load(spec)
+
+        for required_key in ['description', 'version', 'short_name', 'full_name']:
+            if required_key not in self.app_spec:
+                raise Exception(f"Missing '{required_key}' key in app_spec: {app_spec_path}")
+
+        self.log_path = None
+        super(AbstractFlaskApp, self).__init__(self.app_spec["short_name"])
+        self.__initialise__()
+
+    def __initialise__(self):
+        self.log_path = logger.init(self.app_spec["short_name"])
+        logging.info(
+            f'{self.app_spec["full_name"]} ({self.app_spec["short_name"]}), Version: {self.app_spec["version"]}')
+        app_art = text2art(self.app_spec["full_name"])
+        if "organisation" in self.app_spec:
+            print(text2art(self.app_spec["organisation"], "white_bubble"))
+        print(app_art)
+        print(f'Version: {self.app_spec["version"]}')
+        if self.log_path is not None:
+            print(f'Log Path: {self.log_path}')
+        print('\n')
+
 
 class AbstractApp(object):
 
@@ -38,7 +71,8 @@ class AbstractApp(object):
         self.define_args(arg_parser)
 
         self.log_path = logger.init(self.app_spec["short_name"])
-        logging.info(f'{self.app_spec["full_name"]} ({self.app_spec["short_name"]}), Version: {self.app_spec["version"]}')
+        logging.info(
+            f'{self.app_spec["full_name"]} ({self.app_spec["short_name"]}), Version: {self.app_spec["version"]}')
         app_art = text2art(self.app_spec["full_name"])
         if "organisation" in self.app_spec:
             print(text2art(self.app_spec["organisation"], "white_bubble"))
